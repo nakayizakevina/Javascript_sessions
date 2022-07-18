b0d6664dc8b5321657f1370a6ff7d1f053abc56d
kevina <kevinanakayiza@gmail.com>
2022-07-18 11:37:37 +0300

    first attempt of Javascript

---
+++ console.js
@@ -0,0 +1,22 @@
+let a,b,c;
+a=2;
+b=4;
+c=6;
+console.log(a+b+c);
+
+// ooperators
+/* a sign operator (=) this helps to asign values to the variables ie*/
+a=2;
+b=6;
+
+/* compression operators (==) they help to check if values are equal ie*/
+a=2;
+b=2;
+console.log(a==b);
+/*our answer will be true because a and b are equal because they all hold 2 as their value */
+//but if we had 
+
+a=2;
+b=6;
+console.log(a==b);
+/*our answer would be false because both variables hold different values */
